dba5d058e372cd443d5988ba46d29f74738855e6
sairamarao vusirikapally <sairam.970@gmail.com>
2017-09-16 16:44:33 +0530

    Update StudentGroup.java

--- src/StudentGroup.java
+++ src/StudentGroup.java
@@ -45,7 +45,12 @@ public class StudentGroup implements StudentArrayOperation {
 
 	@Override
 	public void setStudent(Student student, int index) {
-		// Add your implementation here
+		if(student == null || index<0 || index >= students.length)
+		throw new IllegalArgumentException("invalid index");
+		else
+		{
+			students[index] = student;
+		}
 	}
 
 	@Override
